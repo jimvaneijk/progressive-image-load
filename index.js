858974d652f7dd14df88b0111a020a339a938a8a
Jim van Eijk <jim@23g.nl>
2018-10-24 16:04:15 +0200

    Fixed package

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-import ProgressiveImageLoad from './src/ProgressiveImageLoad';
+exports.blurImageLoad = function() {
+    const blurImageLoad = document.querySelector('.blur-image-load');
+    const small = blurImageLoad.querySelector('.blur-image');
 
-module.exports = ProgressiveImageLoad.init();
+    // 1: load small image and show it
+    const img = new Image();
+    img.src = small.src;
+    img.onload = () => {
+        small.classList.add('loaded');
+    };
 
+    // 2: load large image
+    const imgLarge = new Image();
+    imgLarge.src = blurImageLoad.dataset.large;
+    imgLarge.onload = () => {
+        imgLarge.classList.add('loaded');
+        imgLarge.classList.add('img-responsive');
+    };
+    blurImageLoad.appendChild(imgLarge);
+};
 
+module.exports = window.onload = blurImageLoad;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+const ProgressiveImageLoad = require('./src/ProgressiveImageLoad');
+
+module.exports = ProgressiveImageLoad;
+
+
